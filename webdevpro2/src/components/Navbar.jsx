ab65022e33f33dcbed82632894dab2fa3dc2c093
Lalit kumar <lalitkumar@Lalits-MacBook-Pro.local>
2025-11-16 17:55:18 +0530

    react second project craeted

---
+++ webdevpro2/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import "./Navbar.css"
+
+function Navbar(){
+    const [open,setOpen] = useState(false);
+
+    return (
+        <nav className="navbar">
+            <div className="logo">My Logo</div>
+
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Services</a></li>
+               <li><a>Contact Us</a></li>
+            </ul>
+
+            <div className="hamburger" onClick={()=>setOpen=(!open)}>
+                 
+            </div>
+        </nav>
+    )
+
+    
+}
+
